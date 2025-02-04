9a5220d1df06ff02e42354293940926e2e7aeff1
Loc Nguyen <nguyenducloc404@gmail.com>
2025-02-04 17:37:23 +0700

    basic crud web

---
+++ ec2/variables.tf
@@ -0,0 +1,23 @@
+variable "key_name" {
+  type    = string
+  default = ""
+}
+
+variable "sg_ids" {
+  type = set(string)
+}
+
+variable "instance_type" {
+  type    = string
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  type    = string
+  default = "my-ec2"
+}
+
+variable "userdata" {
+  type    = string
+  default = ""
+}
